b8fc33adba98edba162ecbb2531597c0c1bba029
hypnorose <yossarian2298@gmail.com>
2022-05-10 16:42:33 +0200

    init commit

--- auth/RtcTokenBuilderSample.py
+++ auth/RtcTokenBuilderSample.py
@@ -9,9 +9,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from auth.RtcTokenBuilder import RtcTokenBuilder,Role_Attendee
 import uuid
 appID = "71813440be5a41f1bda8c8079ff25c45"
-appCertificate = "5d73e34f3ba94c4bb7a99ad3c313dd84"
-uid = 2882341273
-userAccount = "2882341273"
+appCertificate = "FILL_IT"
 expireTimeInSeconds = 3600
 currentTimestamp = int(time.time())
 privilegeExpiredTs = currentTimestamp + expireTimeInSeconds
